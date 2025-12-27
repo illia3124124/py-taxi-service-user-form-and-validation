a93f5c4c71d6a11edfc5f47b42da0ca6f26ccb3d
Illia Mileckiy <illia.miletskyi@oa.edu.ua>
2025-12-27 18:31:42 +0200

    reformatted

--- taxi/forms.py
+++ taxi/forms.py
@@ -17,8 +17,13 @@ class LicenseValidationMixin:
         if len(license_number) != 8:
             raise ValidationError("License number must be 8 characters long")
 
-        if not license_number[:3].isalpha() or not license_number[:3].isupper():
-            raise ValidationError("First 3 characters must be uppercase letters")
+        if (
+                not license_number[:3].isalpha()
+                or not license_number[:3].isupper()
+        ):
+            raise ValidationError(
+                "First 3 characters must be uppercase letters"
+            )
 
         if not license_number[3:].isdigit():
             raise ValidationError("Last 5 characters must be digits")
